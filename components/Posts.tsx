93d941ed9e8d1e5681d140142bc28942c7cefd3c
Mynameistessa <tessabreen@protonmail.com>
2023-10-05 22:23:26 +0100

    Tweak sentence

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -6,7 +6,7 @@ const Posts = () => {
   return (
     <section className='mt-2'>
       <h2 className='font-bold text-2xl dark:text-warm-yellow text-pink-400'> 📕 Blog Posts</h2>
-      <p className='dark:text-white text-base'>These posts are written from the research I did in preparation for lightning talks I presented while working at Unity Technologies.</p>
+      <p className='dark:text-white text-base'>These posts are written from the research I did in preparation for lightning talks.</p>
       <ul className='w-full text-center'>
         {posts.map(post => (
           <ListItem key={post.id} post={post} />
