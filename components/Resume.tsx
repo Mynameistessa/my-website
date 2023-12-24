6bc9f94c0ef3134f582cf8d6bb4b0328ac7f8daa
Mynameistessa <tessabreen@protonmail.com>
2023-12-24 17:04:08 +0000

    Update key and format with prettier

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,8 +1,20 @@
 // import { gsap } from "gsap";
 
-export const ExpandedCVRow = ({ company, role, duration, description, skills, index }:
-  { company: string, role: string, duration: string, description: string[], skills: string[], index: string }) => {
-
+export const ExpandedCVRow = ({
+  company,
+  role,
+  duration,
+  description,
+  skills,
+  index,
+}: {
+  company: string;
+  role: string;
+  duration: string;
+  description: string[];
+  skills: string[];
+  index: string;
+}) => {
   return (
     <div>
       <div className="selector grid grid-cols-3 items-center border-b dark:border-platinum-grey border-wood hover:dark:bg-slate-800 hover:bg-warm-yellow">
@@ -16,28 +28,42 @@ export const ExpandedCVRow = ({ company, role, duration, description, skills, in
           {duration}
         </div>
       </div>
-      <div className={`description-${index} dark:text-white text-neutral-900 px-8 py-4 text-m max-w-200 break-words`}>
+      <div
+        className={`description-${index} dark:text-white text-neutral-900 px-8 py-4 text-m max-w-200 break-words`}
+      >
         <ol className="list-disc max-w-200 break-words">
           {description.map((sentence, index) => (
-            <li key={index}>
-              {sentence}
-            </li>
+            <li key={index}>{sentence}</li>
           ))}
         </ol>
         <div className="xs:invisible flex dark:text-white text-neutral-900 flex-wrap items-end space-x-2 space-y-2 mb-4">
-          {skills.map((skill) => (
-            <span key={Math.random()} className="text-white inline-block whitespace-nowrap rounded-full bg-sky-800 px-[0.65em] pt-[0.35em] pb-[0.25em] text-center align-baseline text-[0.75em] font-bold leading-none">
+          {skills.map((skill, index) => (
+            <span
+              key={index}
+              className="text-white inline-block whitespace-nowrap rounded-full bg-sky-800 px-[0.65em] pt-[0.35em] pb-[0.25em] text-center align-baseline text-[0.75em] font-bold leading-none"
+            >
               {skill}
             </span>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export const CVSmallRow = ({ company, role, duration, description, skills }:
-  { company: string, role: string, duration: string, description: string[], skills: string[] }) => {
+export const CVSmallRow = ({
+  company,
+  role,
+  duration,
+  description,
+  skills,
+}: {
+  company: string;
+  role: string;
+  duration: string;
+  description: string[];
+  skills: string[];
+}) => {
   return (
     <>
       <div className="grid grid-cols-[1fr_1fr] items-center border-b dark:border-platinum-grey border-wood dark:bg-slate-800 bg-warm-yellow">
@@ -48,24 +74,28 @@ export const CVSmallRow = ({ company, role, duration, description, skills }:
           {role}
         </div>
       </div>
-      <div className={`dark:text-white text-neutral-900 px-8 py-2 lg:text-m text-sm max-w-200 break-words`}>
+      <div
+        className={`dark:text-white text-neutral-900 px-8 py-2 lg:text-m text-sm max-w-200 break-words`}
+      >
         <div className="text-end py-2 ml-auto mr-0 text-sm dark:text-brick-red text-neutral-900 whitespace-nowrap italic">
           {duration}
         </div>
         <ol className="list-disc max-w-200 break-words">
           {description.map((sentence, index) => (
-            <li key={index}>
-              {sentence}
-            </li>
+            <li key={index}>{sentence}</li>
           ))}
         </ol>
         <div className="xs:invisible flex dark:text-white text-neutral-900 flex-wrap items-end space-x-2 space-y-2 mb-4">
           {skills.map((skill, index) => (
-            <span key={index} className="text-white inline-block whitespace-nowrap rounded-full bg-sky-800 px-[0.65em] pt-[0.35em] pb-[0.25em] text-center align-baseline text-[0.75em] font-bold leading-none">{skill}</span>
+            <span
+              key={index}
+              className="text-white inline-block whitespace-nowrap rounded-full bg-sky-800 px-[0.65em] pt-[0.35em] pb-[0.25em] text-center align-baseline text-[0.75em] font-bold leading-none"
+            >
+              {skill}
+            </span>
           ))}
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
