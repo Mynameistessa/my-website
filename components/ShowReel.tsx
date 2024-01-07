7f4bff6b840a3b04f2bb4091852a38e7b6811cc9
Mynameistessa <tessabreen@protonmail.com>
2024-01-07 21:04:43 +0000

    Add stagger animation to showreel photos

--- components/ShowReel.tsx
+++ components/ShowReel.tsx
@@ -1,24 +1,31 @@
 "use client";
 import Image from "next/image";
 import { experiences } from "./portfolio";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAnimate, stagger, motion } from "framer-motion";
 
 const ShowReel = () => {
   // Set a fixed height for all images
   const fixedHeight = 200;
   const [photoDescription, setPhotoDescription] = useState<string | null>(null);
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    animate("div", { opacity: 1 }, { delay: stagger(0.3) });
+  }, []);
 
   return (
     <>
-      <div className="flex gap-4 justify-center">
+      <div className="flex gap-4 justify-center" ref={scope}>
         {experiences.map((experience, index) => {
           // Calculate the width based on the fixed height and the aspect ratio
           const width = fixedHeight * experience.aspectRatio;
 
           return (
-            <div
+            <motion.div
               key={index}
-              className=" items-center"
+              initial={{ opacity: 0 }}
+              className="items-center showreel"
               onClick={() => setPhotoDescription(experience.description)}
             >
               <Image
@@ -28,7 +35,7 @@ const ShowReel = () => {
                 height={fixedHeight}
                 className="object-contain inset-0 bg-cover bg-center z-0 hover:drop-shadow-lg" // This will ensure the image fits within the dimensions without stretching or cropping
               />
-            </div>
+            </motion.div>
           );
         })}
       </div>
